a75ed261e8e9a40ea831b6c35499288f6939196a
Kaenguru-Knobel-Kette <manuel.nowack@gmail.com>
2018-12-14 17:48:38 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+registerPatcher({
+    info: info,
+    gameModes: [xelib.gmTES5, xelib.gmSSE],
+    settings: {
+        label: 'Keys Have Weight',
+        templateUrl: `${patcherUrl}/partials/settings.html`,
+        defaultSettings: {
+            weight: 0.1
+        }
+    },
+    requiredFiles: [],
+    getFilesToPatch: function(filenames) {
+        return filenames;
+    },
+    execute: {
+        process: [{
+            load: function(plugin, helpers, settings, locals) {
+                return {
+                    signature: 'KEYM'
+                }
+            },
+            patch: function(record, helpers, settings, locals) {
+                xelib.SetFloatValue(record, 'DATA\\Weight', settings.weight);
+            }
+        }]
+    }
+});
